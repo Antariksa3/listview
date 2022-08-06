1a6391941ea624049c12a5c7ce5c2dad75e4b8b3
Antariksa3 <antariksakusumaharmawan6801@gmail.com>
2022-08-06 17:36:57 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:listview/list.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: List(),
+    );
+  }
+}
---
+++ lib/object.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Object {
+  final String absen;
+  final String nama;
+  final String kelas;
+
+  Object(this.absen, this.nama, this.kelas);
+}
+
+List<Object> datasiswa = [
+  Object("1", "Adhanafi Ilyas Sutejo", "11 PPLG 1"),
+  Object("2", "Ahmad Aziz Wira Widodo", "11 PPLG 1"),
+  Object("3", "Akbar Rizqullah Putra Susanto", "11 PPLG 1"),
+  Object("4", "Alwan Athallah Mumtaz", "11 PPLG 1"),
+  Object("5", "Amri Iqro Samudra Al-Fatihah", "11 PPLG 1"),
+  Object("6", "Anakku Lanang Sejati Adli", "11 PPLG 1"),
+  Object("7", "Andika Setya Eka Natha", "11 PPLG 1"),
+  Object("8", "Antariksa Kusuma Harmawan", "11 PPLG 1"),
+  Object("9", "Azzra Rienov Fahlivi", "11 PPLG 1"),
+  Object("10", "Bayu Septiawan Kurniawan", "11 PPLG 1"),
+  Object("11", "Bhre Nabil Faeyza", "11 PPLG 1"),
+  Object("12", "Bimo Adi Bramantyo", "11 PPLG 1"),
+  Object("13", "Daffa Syauqi Syarif", "11 PPLG 1"),
+  Object("14", "Danar Gading", "11 PPLG 1"),
+  Object("15", "Dimas Bagus Adityas", "11 PPLG 1"),
+  Object("16", "Firdaus Hafidz Al-Kaff", "11 PPLG 1")
+];
